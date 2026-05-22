aa353c3a5820c5b0a0a4e8ec41c26d41657f7a1f
zaberabdullah <zaberabdullah120@gmail.com>
2026-05-22 07:00:33 +0600

    allset

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ const { toNodeHandler } = require("better-auth/node");
 const { getAuth } = require("./lib/auth");
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: [
+    "http://localhost:3000",
+    process.env.CLIENT_URL 
+  ],
   credentials: true,
 }));
 app.use(express.json());
@@ -23,13 +26,19 @@ const startServer = async () => {
     app.all("/api/auth/*splat", toNodeHandler(auth.handler));
 
     app.use("/api/facility", require("./routes/facility"));
-    app.use("/api/booking", require("./routes/booking")); // এই লাইনটা এড করো
+    app.use("/api/booking", require("./routes/booking"));
+
+ 
+    app.get("/", (req, res) => {
+      res.json({ message: "SPORTnest Server Running" });
+    });
 
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
   } catch (err) {
     console.error("CRITICAL STARTUP ERROR:", err);
+    process.exit(1);
   }
 };
 
